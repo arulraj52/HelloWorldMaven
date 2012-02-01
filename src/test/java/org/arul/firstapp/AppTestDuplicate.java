f4dca40cb7b1618f801ccaacb9c15cb0fad856fe
Arulraj Navaraj <arulraj52@yahoo.com>
2012-02-01 14:44:08 -0800

    2nd test code added

--- src/test/java/org/arul/firstapp/AppTestDuplicate.java
+++ src/test/java/org/arul/firstapp/AppTestDuplicate.java
@@ -15,7 +15,7 @@ public class AppTestDuplicate
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public AppTestDuplicate ( String testName )
     {
         super( testName );
     }
@@ -25,7 +25,7 @@ public class AppTestDuplicate
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( AppTestDuplicate.class );
     }
 
     /**
